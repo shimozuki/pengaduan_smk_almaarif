453561f0011f4a9c0b3f7c6129c0e9d8b24278e0
robbishimozuki <robbishimozuki@gmail.com>
2025-07-28 21:46:22 +0800

    fix: Increase maxlength for NIK input field from 16 to 18

--- resources/views/dashboard/users/edit.blade.php
+++ resources/views/dashboard/users/edit.blade.php
@@ -98,7 +98,7 @@
                                             <div class="position-relative">
                                                 <input type="text" class="form-control py-2"
                                                     placeholder="e.g. 1050241708900001" id="nik" name="nik"
-                                                    value="{{ old('nik', $user->nik) }}" maxlength="16" />
+                                                    value="{{ old('nik', $user->nik) }}" maxlength="18" />
                                                 <div class="form-control-icon">
                                                     <i class="bi bi-person-vcard py-2"></i>
                                                 </div>
